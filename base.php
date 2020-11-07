d12463a51e54fe14fdff9b8f8978169d53535f46
potatosmith <liquan.sun@foxmail.com>
2020-11-07 15:11:25 +0800

    插入[排序

--- base.php
+++ base.php
@@ -140,4 +140,21 @@ class Algorithm {
 
         return -1; // 不在数组
     }
+
+    public static function insertSort($arr) {
+        for ($i=0; $i<count($arr); $i++) {
+            $val = $arr[$i];
+            $j = $i-1;
+            while ($j>=0 && $arr[$j] > $val) {
+                $arr[$j+1] = $arr[$j];
+                $j--;
+            }
+            $arr[$j+1] = $val;
+        }
+        return $arr;
+    }
 }
+
+
+
+print_r (Algorithm::insertSort([1,3,6,7,9,2,4,8,5]));
